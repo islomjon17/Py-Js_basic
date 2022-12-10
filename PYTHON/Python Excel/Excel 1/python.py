39e928bca78d1e0d8a96145636acb11450a08ea8
User <turgunovislomjon107@gmail.com>
2022-12-10 06:14:19 +0000

    first time

---
+++ PYTHON/Python Excel/Excel 1/python.py
@@ -0,0 +1,20 @@
+from openpyxl.workbook import Workbook
+from openpyxl import load_workbook
+
+
+
+# Creaate a workbook object
+
+wb = Workbook()
+
+
+# Create an active worksheet
+
+ws = wb.ative
+
+
+
+# load existing spredsheet
+
+
+wb = load_workbook("excal - Copy.xlsx")
